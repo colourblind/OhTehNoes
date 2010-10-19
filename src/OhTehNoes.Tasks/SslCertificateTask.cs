5c2a1360d65268b4049038849d5230b5cf2659a3
colourblind <tom.milsom@gmail.com>
2010-10-19 17:14:49 +0100

    Remove empty elements from list of SSL certs to check

--- src/OhTehNoes.Tasks/SslCertificateTask.cs
+++ src/OhTehNoes.Tasks/SslCertificateTask.cs
@@ -26,26 +26,35 @@ namespace OhTehNoes.Tasks
         {
             WarningThresholdInDays = Int32.Parse(settings.Attributes["warningThreshold"].Value);
             CertsToCheck = settings.Attributes["certsToCheck"].Value.Split(new char[] { ',', ';' }).ToList<string>(); // Take THAT, Law of Demeter
+            CertsToCheck.RemoveAll(o => String.IsNullOrEmpty(o));
         }
 
         public override void Run()
         {
             X509Store store = new X509Store(StoreName.My, StoreLocation.LocalMachine);
             store.Open(OpenFlags.ReadOnly);
+            Dictionary<string, double> certList = null;
 
             foreach (string certName in CertsToCheck)
             {
+                certList = new Dictionary<string, double>();
                 X509Certificate2Collection results = store.Certificates.Find(X509FindType.FindBySubjectName, certName, false);
                 if (results.Count == 0)
                     Logger.Write(this, String.Format("Certificate for {0} missing!", certName), Priority.Warn);
                 else
                 {
-                    double daysRemaining = Double.MinValue;
                     foreach (X509Certificate2 cert in results)
-                        daysRemaining = Math.Max(daysRemaining, cert.NotAfter.Subtract(DateTime.Now).TotalDays);
+                    {
+                        if (!certList.ContainsKey(cert.FriendlyName))
+                            certList[cert.FriendlyName] = Double.MinValue;
+                        certList[cert.FriendlyName] = Math.Max(certList[cert.FriendlyName], cert.NotAfter.Subtract(DateTime.Now).TotalDays);
+                    }
 
-                    if (daysRemaining < WarningThresholdInDays)
-                        Logger.Write(this, String.Format("Certificate for {0} only has {1:n0} days remaining!", certName, daysRemaining), Priority.Warn);
+                    foreach (string key in certList.Keys)
+                    {
+                        if (certList[key] < WarningThresholdInDays)
+                            Logger.Write(this, String.Format("Certificate for {0} only has {1:n0} days remaining!", key, certList[key]), Priority.Warn);
+                    }
                 }
             }
 
